b4b17fb8f1c10c3d1efb56aec739d0684baca57a
Daniel Osborne <1127170+Gwindalmir@users.noreply.github.com>
2020-03-03 07:08:57 -0800

    Bump version

--- WorkshopToolCommon/Constants.cs
+++ WorkshopToolCommon/Constants.cs
@@ -2,7 +2,7 @@
 {
     public static class Constants
     {
-        public const string Version = "0.7.2.*";
+        public const string Version = "0.7.3.*";
 #if SE
         public const string SEWT_Prefix = "[_SEWT_]";
 #else
